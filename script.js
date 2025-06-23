d99ac4adf71c2ce01b9913e17f6bb96930bb8a68
Agustin Bustos Barton <agustinbustosbarton@gmail.com>
2025-06-23 00:21:42 -0300

    betero

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('more-btn');
   const moreContent = document.getElementById('third');
+  const btn2 = document.getElementById('scrolltotop');
 
   btn.addEventListener('mousedown', () => {
     moreContent.classList.remove('hidden');
     moreContent.scrollIntoView({ behavior: 'smooth' });
   });
+btn2.addEventListener('mousedown', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });  });
+
 });
 
 
