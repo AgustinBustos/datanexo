fe0eb1e4cd94b20dc2e391fc823170263d7491d4
Agustin Bustos Barton <agustinbustosbarton@gmail.com>
2025-07-01 20:00:31 -0300

    calendly

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('more-btn');
+  //const btn = document.getElementById('more-btn');
   const moreContent = document.getElementById('third');
   const btn2 = document.getElementById('scrolltotop');
+  const btn3 = document.getElementById('hover-target');
 
-  btn.addEventListener('mousedown', () => {
+btn3.addEventListener('mousedown', () => {
+    name=document.getElementById('fname').value.replaceAll(" ", '%20');
+    email=document.getElementById('lname').value.replaceAll(" ", '%20');
+	console.log(name,email);
+     window.location = "https://calendly.com/agustinbustosbarton/30min?name="+name+"&email="+email;
+  });
+
+/*  btn.addEventListener('mousedown', () => {
     moreContent.classList.remove('hidden');
     moreContent.scrollIntoView({ behavior: 'smooth' });
-  });
+  });  */
 btn2.addEventListener('mousedown', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });  });
 
